03735a902d77a6ae0d0a0e45c83dd0afbb00eea6
luciggl <lucielionascimento19@gmail.com>
2025-01-01 22:04:35 -0300

    feat: Exercicio Dia 1: Nº 2

--- dia1.js
+++ dia1.js
@@ -10,6 +10,16 @@ let idade = 22;
 console.log(`meu nome é ${nome} e tenho ${idade} anos`);
 
 // 2. Declare uma variável para armazenar o preço de um produto e outra para o desconto. Calcule o preço final e mostre no console.
+
+let precoProduto = 250.00;
+let desconto = 0.10;
+
+var resultado = precoProduto -= (precoProduto * desconto);
+
+console.log(`Preço inicial R$: ${precoProduto}
+Desconto de: ${desconto * 100}
+Preço final: ${resultado}`);
+
 // 3. Crie um array com três de suas comidas favoritas e exiba a segunda comida na lista.
 // 4. Crie um objeto que represente um livro, com propriedades como `titulo`, `autor` e `ano`. Mostre no console o título e o autor.
 // 5. Declare uma variável `estaLogado` com o valor `false`. Depois, mude o valor para `true` e imprima a mensagem: "Status de login: [valor da variável]".
