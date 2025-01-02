4cd976ae9479b119e66e8418fa5ce1ddf9fb419d
luciggl <lucielionascimento19@gmail.com>
2025-01-01 22:04:07 -0300

    feat: Exercicio Dia 1: Nº 1

---
+++ dia1.js
@@ -0,0 +1,16 @@
+// ### **🎯 Exercícios Práticos**
+
+// Agora é a sua vez! 💪 Resolva os exercícios abaixo:
+
+// 1. Crie uma variável para armazenar seu nome e outra para sua idade. Imprima uma frase como: "Meu nome é [nome] e tenho [idade] anos".
+
+let nome = "Luciélio";
+let idade = 22;
+
+console.log(`meu nome é ${nome} e tenho ${idade} anos`);
+
+// 2. Declare uma variável para armazenar o preço de um produto e outra para o desconto. Calcule o preço final e mostre no console.
+// 3. Crie um array com três de suas comidas favoritas e exiba a segunda comida na lista.
+// 4. Crie um objeto que represente um livro, com propriedades como `titulo`, `autor` e `ano`. Mostre no console o título e o autor.
+// 5. Declare uma variável `estaLogado` com o valor `false`. Depois, mude o valor para `true` e imprima a mensagem: "Status de login: [valor da variável]".
+// 6. Crie uma variável constante chamada `PI` com o valor `3.14`. Use-a para calcular a área de um círculo com raio 5 e mostre o resultado.
