9c931a931110ddc6a5f032cb5b51bbaf01ef0f8d
luciggl <lucielionascimento19@gmail.com>
2025-01-05 23:15:59 -0300

    fix: Exercicio Dia 5: Nº 3

--- dia5.js
+++ dia5.js
@@ -15,6 +15,16 @@ for(let j = 0; j < pessoas.length; ++j){
 }
 
 // 3. Escreva um código que verifica se um número em um array é maior que 10. Pare de verificar assim que encontrar o primeiro.
+
+const arrayNumeros = [1, 2, 3, 5,10, 12, 20]
+
+for(let i = 0; i < arrayNumeros.length; i++){
+    if(arrayNumeros[i] > 10){
+        console.log(`Primeiro numero maior que 10 encontrado no array foi ${arrayNumeros[i]}`)
+        break
+    }
+}
+
 // 4. Use um `while` para simular um contador de 10 a 0 e exiba "Lançamento!" quando atingir 0.
 // 5. Com um `do...while`, simule um caixa eletrônico pedindo a senha até que ela esteja correta.
 // 6. Escreva um loop que calcula o fatorial de um número (ex: `5! = 5*4*3*2*1`).
