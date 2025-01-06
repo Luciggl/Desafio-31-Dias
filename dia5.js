86a2ef19ee8398074f0339f1bc741d994108975a
luciggl <lucielionascimento19@gmail.com>
2025-01-05 23:07:04 -0300

    fix: Exercicio Dia 5: Nº 1

---
+++ dia5.js
@@ -0,0 +1,13 @@
+// ### **🎯 Exercícios Práticos**
+
+// 1. Use um loop `for` para imprimir os números de 1 a 10 no console.
+
+for(let i = 1; i <=10; i++){
+    console.log(i)
+}
+
+// 2. Crie uma lista de nomes e use um `for` para exibir cada nome.
+// 3. Escreva um código que verifica se um número em um array é maior que 10. Pare de verificar assim que encontrar o primeiro.
+// 4. Use um `while` para simular um contador de 10 a 0 e exiba "Lançamento!" quando atingir 0.
+// 5. Com um `do...while`, simule um caixa eletrônico pedindo a senha até que ela esteja correta.
+// 6. Escreva um loop que calcula o fatorial de um número (ex: `5! = 5*4*3*2*1`).
