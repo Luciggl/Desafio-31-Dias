dc8ba839ed0f19c84431d2f86b933dde607f00cf
luciggl <lucielionascimento19@gmail.com>
2025-01-05 23:11:23 -0300

    fix: Exercicio Dia 5: Nº 2

--- dia5.js
+++ dia5.js
@@ -7,6 +7,13 @@ for(let i = 1; i <=10; i++){
 }
 
 // 2. Crie uma lista de nomes e use um `for` para exibir cada nome.
+
+const pessoas = ["joão", "maria", "patricia", "pedro"]
+
+for(let j = 0; j < pessoas.length; ++j){
+    console.log(pessoas[j])
+}
+
 // 3. Escreva um código que verifica se um número em um array é maior que 10. Pare de verificar assim que encontrar o primeiro.
 // 4. Use um `while` para simular um contador de 10 a 0 e exiba "Lançamento!" quando atingir 0.
 // 5. Com um `do...while`, simule um caixa eletrônico pedindo a senha até que ela esteja correta.
