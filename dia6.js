3ffe9175e5d277c3c112ce5877c85d3fad7cbb63
luciggl <lucielionascimento19@gmail.com>
2025-01-06 21:24:45 -0300

    fix: Exercicio Dia 6: Nº 1

---
+++ dia6.js
@@ -0,0 +1,12 @@
+// ### **🎯 Exercícios Práticos**
+
+// 1. Crie um array com suas três cores favoritas e adicione uma nova cor no final.
+
+const cores = ["azul", "amarelo", "vermelho"]
+cores.push("roxo")
+
+// 2. Use um loop para exibir todos os itens de um array de compras.
+// 3. Dado um array de números `[10, 20, 30, 40, 50]`, use o método `slice` para extrair os dois últimos números.
+// 4. Crie um array de tarefas e remova a primeira tarefa usando `shift`.
+// 5. Use `splice` para substituir o terceiro item de um array por "Substituído".
+// 6. Transforme um array de palavras em uma frase completa usando `join`.
