6fad5f603f46d83d77d6d7ea3be3def61f6bd4e7
luciggl <lucielionascimento19@gmail.com>
2025-01-07 20:34:34 -0300

    fix: Exercicio Dia 7: Nº 3

--- dia7.js
+++ dia7.js
@@ -9,13 +9,21 @@ console.log(calcularDobroNumero(9))
 
 // 2. Escreva uma função que receba um nome e retorne uma mensagem de boas-vindas.
 
-daBoasVindas = (nome) => {
+const daBoasVindas = (nome) => {
     return `Olá, ${nome} seja Bem Vindo!`
 }
 
 console.log(daBoasVindas("João"))
 
 // 3. Crie uma função que receba dois números e retorne o maior deles.
+
+const verificarMaiorNumero = (numero1, numero2) => {
+    let maior = numero1 > numero2 ? numero1 : numero2
+    return maior
+}
+
+console.log(verificarMaiorNumero(10, 8))
+
 // 4. Implemente uma função que calcule a média de três números.
 // 5. Crie uma função que receba um array de números e retorne a soma deles.
 // 6. Escreva uma função que receba uma string e retorne o número de caracteres dessa string.
