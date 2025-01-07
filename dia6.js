6afedaadf7b011f4bd8c156d26871020fb1c912f
luciggl <lucielionascimento19@gmail.com>
2025-01-06 21:27:16 -0300

    fix: Exercicio Dia 6: Nº 2

--- dia6.js
+++ dia6.js
@@ -6,6 +6,12 @@ const cores = ["azul", "amarelo", "vermelho"]
 cores.push("roxo")
 
 // 2. Use um loop para exibir todos os itens de um array de compras.
+
+const compras = ["arroz", "feijão", "macarrão", "batata", "coca-cola", "leite", "pão"]
+for(compra in compras){
+    console.log(compras[compra])
+}
+
 // 3. Dado um array de números `[10, 20, 30, 40, 50]`, use o método `slice` para extrair os dois últimos números.
 // 4. Crie um array de tarefas e remova a primeira tarefa usando `shift`.
 // 5. Use `splice` para substituir o terceiro item de um array por "Substituído".
