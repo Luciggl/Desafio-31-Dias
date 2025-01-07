44594425b85cc8fdc15ad89483b0fadda1f780cd
luciggl <lucielionascimento19@gmail.com>
2025-01-06 21:30:52 -0300

    fix: Exercicio Dia 6: Nº 3

--- dia6.js
+++ dia6.js
@@ -13,6 +13,10 @@ for(compra in compras){
 }
 
 // 3. Dado um array de números `[10, 20, 30, 40, 50]`, use o método `slice` para extrair os dois últimos números.
+
+const numeros = [10, 20, 30, 40, 50]
+numeros.splice(3, 2)
+console.log(numeros)
 // 4. Crie um array de tarefas e remova a primeira tarefa usando `shift`.
 // 5. Use `splice` para substituir o terceiro item de um array por "Substituído".
 // 6. Transforme um array de palavras em uma frase completa usando `join`.
