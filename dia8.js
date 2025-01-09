d6d3b31635f8c893d23261135df2e33a6b744447
luciggl <lucielionascimento19@gmail.com>
2025-01-08 21:38:28 -0300

    fix: Exercicio Dia 8: Nº 3

--- dia8.js
+++ dia8.js
@@ -5,9 +5,9 @@
 const matriz = [];
 let contador = 1;
 
-for (let i = 0; i < 3; i++){
+for (let i = 0; i < 3; i++) {
     const linha = [];
-    for(let j = 0; j < 3; j++){
+    for (let j = 0; j < 3; j++) {
         linha.push(contador++);
     }
     matriz.push(linha);
@@ -19,8 +19,8 @@ console.log(matriz)
 
 const somaMatriz = (matrizBase) => {
     let soma = 0
-    for(let i = 0; i<matrizBase.length; i++){
-        for(let j=0; j<matrizBase[i].length; j++){
+    for (let i = 0; i < matrizBase.length; i++) {
+        for (let j = 0; j < matrizBase[i].length; j++) {
             soma += matrizBase[i][j];
         }
     }
@@ -30,6 +30,25 @@ const somaMatriz = (matrizBase) => {
 console.log(somaMatriz(matriz))
 
 // 3. Crie um tabuleiro de xadrez 8x8, preenchendo-o com "⬜" e "⬛".
+
+const criarTabulerio = () => {
+    let tabuleiro = ''
+    for (let i = 0; i < 8; i++) {
+        let linha = ''
+        for (let j = 0; j < 8; j++) {
+            if ((i + j) % 2 === 0) {
+                linha += '⬜'
+            } else {
+                linha += '⬛'
+            }
+        }
+        tabuleiro += linha + "\n";
+    }
+    return tabuleiro;
+};
+
+console.log(criarTabulerio())
+
 // 4. Verifique quantos números em uma matriz são pares.
 // 5. Substitua todos os valores maiores que 10 em uma matriz por `0`.
 // 6. Implemente uma função que multiplique todos os elementos de uma matriz por um valor fornecido.
