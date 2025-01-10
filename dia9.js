7b3fc88c23dd00ce2cf837e0a5af95d32583aee6
luciggl <lucielionascimento19@gmail.com>
2025-01-09 23:17:47 -0300

    fix: Exercicio Dia 9: Nº 2

--- dia9.js
+++ dia9.js
@@ -3,15 +3,26 @@
 // 1. Escreva uma função recursiva que conte de `n` até 0.
 
 const contarDeNaZero = (numero) => {
-    if(numero === 0){
+    if (numero === 0) {
         return console.log("0")
     }
     console.log(numero)
-    contarDeNaZero(numero-1)
+    contarDeNaZero(numero - 1)
 }
 
 contarDeNaZero(10)
 // 2. Implemente uma função que calcule o fatorial de um número.
+
+const calcularFatorial = (numero) => {
+    let resultadoFatorial = 1;
+    if (numero === 0) {
+        return 1;
+    }
+    return numero * calcularFatorial(numero - 1)
+}
+
+console.log(calcularFatorial(5))
+
 // 3. Crie uma função que some todos os números de um array usando recursão.
 // 4. Escreva uma função para verificar se uma palavra é um palíndromo usando recursão.
 // 5. Implemente uma função que calcule o n-ésimo termo da sequência de Fibonacci.
